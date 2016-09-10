e74af5c3c4a2d495a73e01a585ceead88b26ac86
sssnakes <finnan.barry@gmail.com>
2016-09-11 00:51:22 +0200

    arial stage 1

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 
   // Top fold out on mouse over
   $('.h-banner').mouseenter(function() {
-    $('.h-banner').animate({
+    $('.slide-wrap').animate({
       top: 0
-    }, 200, function(){});
+    }, 200, function(){} );
   }).mouseleave(function() {
-    $('.h-banner').animate({
-      top: -365
-    }, 200, function(){});
+    $('.slide-wrap').animate({
+      top: -385
+    }, 200, function(){} );
   });
 
 
