1b3a8d06e9b99a2caaa73241521f49867c50f19a
sssnakes <finnan.barry@gmail.com>
2016-10-04 21:02:00 +0200

    0.10

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
-
-  // Top fold out on mouse over
-  $('.h-banner').mouseenter(function() {
-    $('.slide-wrap').animate({
-      top: 0
-    }, 200, function(){} );
-  }).mouseleave(function() {
-    $('.slide-wrap').animate({
-      top: -385
-    }, 200, function(){} );
-  });
+  // OLD AND UNUSED NOW
+    // Top fold out on mouse over
+      // $('.h-banner').mouseenter(function() {
+      //   $('.slide-wrap').animate({
+      //     top: 0
+      //   }, 200, function(){} );
+      // }).mouseleave(function() {
+      //   $('.slide-wrap').animate({
+      //     top: -385
+      //   }, 200, function(){} );
+      // });
 
 
 });
