5f534943cc3616ebaf95f4bb90782fef9f48de11
sssnakes <finnan.barry@gmail.com>
2016-10-05 11:46:13 +0200

    0.11

--- js/app.js
+++ js/app.js
@@ -13,4 +13,31 @@ $(document).ready(function() {
       // });
 
 
+  // HOVER SLIDE, LEFT CONTAINER
+  $('.scroll-bar-container').mouseenter(function() {
+    console.log('mouseenter');
+    $('.right-m-container').animate({
+      left: 345
+    }, 200, function(){} );
+  }).mouseleave(function() {
+    console.log('mouseleave');
+    $('.right-m-container').animate({
+      left: 150
+    }, 200, function(){} );
+  });
+
+  // HOVER SLIDE, RIGHT CONTAINER
+  $('.info-slide').mouseenter(function() {
+    console.log('mouseenter');
+    $('.container').animate({
+      right: 345
+    }, 200, function(){} );
+  }).mouseleave(function() {
+    console.log('mouseleave');
+    $('.container').animate({
+      right: 150
+    }, 200, function(){} );
+  });
+
+
 });
